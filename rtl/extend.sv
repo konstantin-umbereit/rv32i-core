7206f35373f0750876fe3f31f9516a51db77540a
Konstantin Umbereit <konstantin.umbereit@proton.me>
2026-07-11 14:02:29 +0200

    rtl(extend.sv): fixed U, J, B-type cases

--- rtl/extend.sv
+++ rtl/extend.sv
@@ -16,40 +16,42 @@
         case (imm_src)
             /* I-type */
             3'b000: begin
-                imm_ext[11:0]  = instr[31:20];
-                imm_ext[31:12] = {20{instr[31]}}; 
+                imm_ext[31:12] = {20{instr[31]}};
+                imm_ext[11:0]  = instr[31:20]; 
             end 
 
             /* S-type */
             3'b001: begin
+                imm_ext[31:12] = {20{instr[31]}};
+                imm_ext[11:5]  = instr[31:25]; 
                 imm_ext[4:0]   = instr[11:7];
-                imm_ext[11:5]  = instr[31:25];
-                imm_ext[31:12] = {20{instr[31]}}; 
             end
 
             /* B-type */
             3'b010: begin
-                imm_ext[3:0]   = instr[11:8];
-                imm_ext[9:4]   = instr[30:25];
-                imm_ext[10]    = instr[7];
-                imm_ext[11]    = instr[31];
-                imm_ext[31:12] = {20{instr[31]}};
+                imm_ext[31:13] = {19{instr[31]}};
+                imm_ext[12]    = instr[31];
+                imm_ext[11]    = instr[7];
+                imm_ext[10:5]  = instr[30:25];
+                imm_ext[4:1]   = instr[11:8];
+                imm_ext[0]     = 1'b0;
+                
             end
 
             /* U-type */
             3'b011: begin
-                imm_ext[19:0]  = instr[31:12];
-                imm_ext[31:20] = {12{instr[31]}};
+                imm_ext[31:12] = instr[31:12];
+                imm_ext[11:0]  = 12'b0;
             end
 
             /* J-type */
             3'b100: begin
-                imm_ext[9:0]   = instr[30:21];
-                imm_ext[10]    = instr[20];
-                imm_ext[18:11] = instr[19:12];
-                imm_ext[19]    = instr[31];
-                imm_ext[31:20] = {12{instr[31]}};
-
+                imm_ext[31:21] = {11{instr[31]}};
+                imm_ext[20]    = instr[31];
+                imm_ext[19:12] = instr[19:12];
+                imm_ext[11]    = instr[20];
+                imm_ext[10:1]  = instr[30:21];
+                imm_ext[0]     = 1'b0;
             end
             default: begin
                 imm_ext[DATA_WIDTH-1:0] = 32'b0;
