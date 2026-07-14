9ac799c354207bf606c10e823f54b8fadab13fcf
Konstantin Umbereit <konstantin.umbereit@proton.me>
2026-07-15 00:16:53 +0200

    rtl(instr_memory.sv): changed addressability from 8bit to 32bit to allow instruction loading with $readmemh

--- rtl/instr_memory.sv
+++ rtl/instr_memory.sv
@@ -6,17 +6,17 @@
  module instr_memory #(
     parameter DATA_WIDTH = 32,
     parameter ADDR_WIDTH = 32,
-    parameter MEM_SIZE = 1024
+    parameter MEM_SIZE = 1024 /* 4KB instruction memory */
  ) (
     input  logic [ADDR_WIDTH-1:0] pc,           /* PC */
 
     output logic [DATA_WIDTH-1:0] instr         /* instruction output */
  );
     /* Memory: 1024 x 8bit */
-    logic [7:0] mem [0:MEM_SIZE-1];
+    logic [DATA_WIDTH-1:0] mem [0:MEM_SIZE-1];
 
     /* Instruction output */
-    assign instr = {mem[pc+3],mem[pc+2],mem[pc+1],mem[pc]};
+    assign instr = mem[pc[$clog2(MEM_SIZE)+1 : 2] ]; /* calculates the amount of bits needed to address a MEMSIZE large array */
     
  endmodule
  
